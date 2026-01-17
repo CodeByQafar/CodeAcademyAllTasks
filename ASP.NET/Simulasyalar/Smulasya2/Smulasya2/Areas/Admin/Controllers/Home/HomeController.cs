146013add39c01e5b5cc48a603f0923658e05b4d
CodeByQafar <megrurniftiyev@gmail.com>
2026-01-17 16:52:11 +0400

    similation 2 is fninshed

--- ASP.NET/Simulasyalar/Smulasya2/Smulasya2/Areas/Admin/Controllers/Home/HomeController.cs
+++ ASP.NET/Simulasyalar/Smulasya2/Smulasya2/Areas/Admin/Controllers/Home/HomeController.cs
@@ -12,293 +12,192 @@ namespace Smulasya2.Areas.Admin.Controllers.Home
     [Area("Admin")]
     public class HomeController : Controller
     {
-        public readonly AppDbContext _Context;
+
+        public readonly AppDbContext _context;
         public readonly IWebHostEnvironment _env;
-
-        public HomeController(AppDbContext context, IWebHostEnvironment env )
+        public HomeController(AppDbContext context, IWebHostEnvironment env)
         {
-            _Context = context;
+            _context = context;
             _env = env;
-        }
-
-
-        public IActionResult Create(CreateVM createVM)
-        {
-            if (!ModelState.IsValid){
-                return View(createVM);
-            }
-            if (createVM.ImageFile == null)
-            {
-                ModelState.AddModelError("ImageFile",  "ImageFile doesnt exist");
-                return View(createVM);
-
-            }
-            if (!createVM.ImageFile.ContentType.Contains("image/"))
-            {
-                ModelState.AddModelError("ImageFile", "FileType Invalid");
-                return View(createVM);
-
-            }
-            if (createVM.ImageFile.Length >= 2 * 1024 * 1024) 
-            {
-                ModelState.AddModelError("ImageFile", "ImageFile Size is More Big than 2MB");
-                return View(createVM);
-            }
-            string filename = string.Concat(Guid.NewGuid().ToString(), createVM.ImageFile.FileName);
-            string path = Path.Combine(_env.WebRootPath, "img", filename);
-            Stream stream = new FileStream(path, FileMode.Create);
-            createVM.
-         List<Member> members = _Context.Members.ToList();
-            return View(members);
         }
         public IActionResult Index()
         {
-            List<Member> members = _Context.Members.ToList();
+            List<Member> members = _context.Members.ToList();
+
             return View(members);
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //    public readonly AppDbContext _context;
-        //    public readonly IWebHostEnvironment _env;
-        //    public HomeController(AppDbContext context, IWebHostEnvironment env)
-        //    {
-        //        _context = context;
-        //        _env = env;
-        //    }
-        //    public IActionResult Index()
-        //    {
-        //        List<Member> members = _context.Members.ToList();
-
-        //        return View(members);
-        //    }
-        //    public IActionResult Create()
-        //    {
-
-        //        return View();
-        //    }
-        //    [HttpPost]
-        //    public async Task<IActionResult> Create(CreateVM createVM)
-        //    {
-
-        //        if (createVM.ImageFile == null)
-        //        {
-        //            ModelState.AddModelError("ImageFile", "Image can't be null");
-        //            return View(createVM);
-        //        }
-        //        else if (!createVM.ImageFile.ContentType.Contains("image/"))
-        //        {
-        //            ModelState.AddModelError("ImageFile", "image format is invaild");
-        //            return View(createVM);
-
-        //        }
-        //        else if (createVM.ImageFile.Length > 2 * 1024 * 1024)
-        //        {
-        //            ModelState.AddModelError("ImageFile", "image size is must be lower than 2mb");
-
-        //            return View(createVM);
-
-        //        }
-        //        if (!ModelState.IsValid)
-        //        {
-        //            return View(createVM);
-        //        }
-        //        string filename = String.Concat(Guid.NewGuid().ToString(), createVM.ImageFile.FileName);
-        //        string path = Path.Combine(_env.WebRootPath, "img", filename);
-        //        Stream stream = new FileStream(path, FileMode.Create);
-        //        await createVM.ImageFile.CopyToAsync(stream);
-        //        stream.Close();
-
-        //        Member member = new Member
-        //        {
-        //            CreatedAt = DateTime.Now,
-        //            FullName = createVM.FullName,
-        //            Position = createVM.Position,
-        //            isDeletet=false,
-        //            ImageUrl = filename,
-        //        };
-        //        _context.Members.Add(member);
-        //        _context.SaveChanges();
-        //        return RedirectToAction("index","home");
-        //    }
-
-        //    public IActionResult Detail(int? id)
-        //    {
-        //        if (id == null)
-        //        {
-        //            return BadRequest();            }
-
-        //        Member member = _context.Members.FirstOrDefault(p => p.Id == id);
-        //         if (member==null)
-        //        {
-        //            return BadRequest();
-        //        }
-        //        DetailVM detailVM = new DetailVM
-        //        {
-        //            FullName = member.FullName,
-        //            Position = member.Position,
-        //            ImageUrl = member.ImageUrl
-        //        };
-        //            return View(detailVM);
-        //    }
-        //    public IActionResult Update(int? id)
-        //    {
-        //        if (id == null)
-        //        {
-        //            return BadRequest();
-        //        }
-
-        //        Member member = _context.Members.FirstOrDefault(p => p.Id == id);
-        //        if (member == null)
-        //        {
-        //            return BadRequest();
-        //        }
-        //        UpdateVM updateVM = new UpdateVM
-        //        {
-        //            FullName = member.FullName,
-        //            Position = member.Position,
-        //            ImageUrl = member.ImageUrl,
-
-        //        };
-
-        //        return View(updateVM);
-        //    }
-        //    [HttpPost]
-        //    public async Task< IActionResult> Update(int? id,UpdateVM updateVM)
-        //    {
-        //        if (id == null)
-        //        {
-        //            return BadRequest();
-        //        }
-
-        //        Member member = _context.Members.FirstOrDefault(p => p.Id == id);
-        //        if (member == null)
-        //        {
-        //            return BadRequest();
-        //        }
-        //        if (updateVM.ImageFile != null)
-        //        {
-        //            if (!updateVM.ImageFile.ContentType.Contains("image/"))
-        //            {
-        //                ModelState.AddModelError("ImageFile", "image format is invaild");
-        //                return View(updateVM);
-
-        //            }
-        //            else if (updateVM.ImageFile.Length > 2 * 1024 * 1024)
-        //            {
-        //                ModelState.AddModelError("ImageFile", "image size is must be lower than 2mb");
-
-        //                return View(updateVM);
-
-        //            }
-        //            else
-        //            { 
-        //                if (!ModelState.IsValid)
-        //                {
-        //                    return View(updateVM);
-        //                }
-        //                string filename = String.Concat(Guid.NewGuid().ToString(), updateVM.ImageFile.FileName);
-        //                string path = Path.Combine(_env.WebRootPath, "img", filename);
-        //                Stream stream = new FileStream(path, FileMode.Create);
-        //                await updateVM.ImageFile.CopyToAsync(stream);
-        //                stream.Close();
-
-
-        //                member.Position = updateVM.Position;
-        //                member.FullName = updateVM.FullName;
-        //                member.ImageUrl = filename;
-        //                _context.Members.Update(member);
-        //                _context.SaveChanges();
-        //                return RedirectToAction("index", "home");
-        //            }
-        //        }
-
-        //        else
-        //        {
-        //            member.Position = updateVM.Position;
-        //            member.FullName = updateVM.FullName;
-
-
-        //            _context.Members.Update(member);
-        //            return RedirectToAction("index", "home");
-
-        //        }
-
-
-
-        //    }
-
-        //    public IActionResult Delete(int ? id)
-        //    {
-        //        if (id == null)
-        //        {
-        //            return BadRequest();
-        //        }
-
-        //        Member member = _context.Members.FirstOrDefault(p => p.Id == id);
-        //        if (member == null)
-        //        {
-        //            return BadRequest();
-        //        }
-        //        _context.Members.Remove(member);
-        //        _context.SaveChanges();
-        //        return RedirectToAction("index","home");
-        //    }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateVM createVM)
+        {
+
+            if (createVM.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "Image can't be null");
+                return View(createVM);
+            }
+            else if (!createVM.ImageFile.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("ImageFile", "image format is invaild");
+                return View(createVM);
+
+            }
+            else if (createVM.ImageFile.Length > 2 * 1024 * 1024)
+            {
+                ModelState.AddModelError("ImageFile", "image size is must be lower than 2mb");
+
+                return View(createVM);
+
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(createVM);
+            }
+            string filename = String.Concat(Guid.NewGuid().ToString(), createVM.ImageFile.FileName);
+            string path = Path.Combine(_env.WebRootPath, "img", filename);
+            Stream stream = new FileStream(path, FileMode.Create);
+            await createVM.ImageFile.CopyToAsync(stream);
+            stream.Close();
+
+            Member member = new Member
+            {
+                CreatedAt = DateTime.Now,
+                FullName = createVM.FullName,
+                Position = createVM.Position,
+                isDeletet = false,
+                ImageUrl = filename,
+            };
+            _context.Members.Add(member);
+            _context.SaveChanges();
+            return RedirectToAction("index", "home");
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            Member member = _context.Members.FirstOrDefault(p => p.Id == id);
+            if (member == null)
+            {
+                return BadRequest();
+            }
+            DetailVM detailVM = new DetailVM
+            {
+                FullName = member.FullName,
+                Position = member.Position,
+                ImageUrl = member.ImageUrl
+            };
+            return View(detailVM);
+        }
+        public IActionResult Update(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            Member member = _context.Members.FirstOrDefault(p => p.Id == id);
+            if (member == null)
+            {
+                return BadRequest();
+            }
+            UpdateVM updateVM = new UpdateVM
+            {
+                FullName = member.FullName,
+                Position = member.Position,
+                ImageUrl = member.ImageUrl,
+
+            };
+
+            return View(updateVM);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(int? id, UpdateVM updateVM)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            Member member = _context.Members.FirstOrDefault(p => p.Id == id);
+            if (member == null)
+            {
+                return BadRequest();
+            }
+            if (updateVM.ImageFile != null)
+            {
+                if (!updateVM.ImageFile.ContentType.Contains("image/"))
+                {
+                    ModelState.AddModelError("ImageFile", "image format is invaild");
+                    return View(updateVM);
+
+                }
+                else if (updateVM.ImageFile.Length > 2 * 1024 * 1024)
+                {
+                    ModelState.AddModelError("ImageFile", "image size is must be lower than 2mb");
+
+                    return View(updateVM);
+
+                }
+                else
+                {
+                    if (!ModelState.IsValid)
+                    {
+                        return View(updateVM);
+                    }
+                    string filename = String.Concat(Guid.NewGuid().ToString(), updateVM.ImageFile.FileName);
+                    string path = Path.Combine(_env.WebRootPath, "img", filename);
+                    Stream stream = new FileStream(path, FileMode.Create);
+                    await updateVM.ImageFile.CopyToAsync(stream);
+                    stream.Close();
+
+
+                    member.Position = updateVM.Position;
+                    member.FullName = updateVM.FullName;
+                    member.ImageUrl = filename;
+                    _context.Members.Update(member);
+                    _context.SaveChanges();
+                    return RedirectToAction("index", "home");
+                }
+            }
+
+            else
+            {
+                member.Position = updateVM.Position;
+                member.FullName = updateVM.FullName;
+
+
+                _context.Members.Update(member);
+                return RedirectToAction("index", "home");
+
+            }
+
+
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            Member member = _context.Members.FirstOrDefault(p => p.Id == id);
+            if (member == null)
+            {
+                return BadRequest();
+            }
+            _context.Members.Remove(member);
+            _context.SaveChanges();
+            return RedirectToAction("index", "home");
+        }
     }
 }
--- ASP.NET/Simulasyalar/Smulasya2/Smulasya2/Controllers/Account/AccountController.cs
+++ ASP.NET/Simulasyalar/Smulasya2/Smulasya2/Controllers/Account/AccountController.cs
@@ -14,46 +14,91 @@ namespace Smulasya2.Controllers.Account
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
+        public IActionResult Login( )
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
         public async Task<IActionResult> Register(RegisterVM registerVM)
         {
             if (!ModelState.IsValid)
             {
-                return View( registerVM);
+                return View(registerVM);
             }
             AppUser? user = await _userManager.FindByEmailAsync(registerVM.Email);
             if (user != null)
             {
                 ModelState.AddModelError("Email", "this Email used on other account");
+                return View(registerVM);
             }
             user = await _userManager.FindByNameAsync(registerVM.UserName);
             if (user != null)
             {
                 ModelState.AddModelError("UserName", "this UserName used on other account");
+                return View(registerVM);
+
             }
             AppUser newUser = new AppUser
             {
                 FullName = registerVM.FullName,
-                UserName=registerVM.UserName,
-                Email=registerVM.Email,
-                
+                UserName = registerVM.UserName,
+                Email = registerVM.Email,
+
             };
-           IdentityResult result=await _userManager.CreateAsync(newUser,registerVM.Password);
+            IdentityResult result = await _userManager.CreateAsync(newUser, registerVM.Password);
             if (!result.Succeeded)
             {
                 foreach (IdentityError error in result.Errors)
                 {
                     ModelState.AddModelError("", error.Description);
+                    Console.WriteLine(error.Description);
                 }
+                return View(registerVM);
+
             }
             await _signInManager.SignInAsync(newUser, false);
 
-            return RedirectToAction("index","home");
+            return RedirectToAction("index", "home");
         }
-        public IActionResult Login(LoginVM loginVM)
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
         {
-            return View();
+            if(!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+            AppUser? user = await _userManager.FindByNameAsync(loginVM.UserNameOrEmail);
+            if (user == null)
+            {
+               
+                user = await _userManager.FindByEmailAsync(loginVM.UserNameOrEmail);
+                if (user == null)
+                {
+                    ModelState.AddModelError("UserNameOrEmail", "user not found");
+                    return View(loginVM);
+                }
+            }
+            var result =await _signInManager.PasswordSignInAsync(user, loginVM.Password, loginVM.RememberMe, false);
+            if (result.Succeeded)
+            {
+
+                return RedirectToAction("index", "home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Username or password is incorrect");
+                return View(loginVM);
+
+            }
         }
-        public async Task<IActionResult> Logout( )
+        public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
 
