5976acb1fee1b930a02934484f6d63ffa531f4ef
CodeByQafar <megrurniftiyev@gmail.com>
2026-01-21 14:29:13 +0400

    new

--- ASP.NET/Simulasyalar/Simulasya3/Simulasya3/obj/Debug/net8.0/Simulasya3.AssemblyInfo.cs
+++ ASP.NET/Simulasyalar/Simulasya3/Simulasya3/obj/Debug/net8.0/Simulasya3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simulasya3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279e8e111722234d119d1b34fcf2667d6f6aefa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simulasya3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simulasya3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
